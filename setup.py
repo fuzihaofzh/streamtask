a9c1506038115f2c76592eca49b8eb4d92f36ee7
Zihao <zhfu@cam.ac.uk>
2022-10-09 11:55:06 +0100

    modify pip install to work

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
-from version import __version__
+from streamtask.version import __version__
 
 setup(
     name='streamtask',
